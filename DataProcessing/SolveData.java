1e144d1d162e17efc0e1d4dcdacc248e93ffaacc
lewin349 <lewinl349@gmail.com>
2022-11-19 00:07:12 -0500

    Added base class of solving data

---
+++ DataProcessing/SolveData.java
@@ -0,0 +1,24 @@
+package DataProcessing;
+import java.util.ArrayList;
+
+public class SolveData {
+    /**
+     * 1. Receive (Constructor)
+     * 2. Create list for all variables present (Note: maybe some seperate methods)
+     * 3. Convert the equations into matrix form (Note: Probably many mini methods here too)
+     * 4. Set it through the Guassian equation
+     * 5. Export an answer list
+     */
+    private ArrayList<ArrayList<String>> equations;
+    private ArrayList<String> variables;
+
+    public SolveData(ArrayList<ArrayList<String>> equations) {
+        this.equations = equations;
+    }
+
+    
+
+    private void getVariables() {
+
+    }
+}
