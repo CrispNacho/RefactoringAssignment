db587b95702bbafe5046a6474af0280e1b32ce88
Oblivion Says Hello <lewinl349@gmail.com>
2022-11-21 15:12:28 -0500

    Removing Cleanse data method

--- DataProcessing/ReadQuestionAndAnswerData.java
+++ DataProcessing/ReadQuestionAndAnswerData.java
@@ -1,13 +1,12 @@
 package DataProcessing;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class ReadQuestionAndAnswerData extends ReadData{
     private int questionCount = -1;
     protected void addContentLine(String contentLine){
         if(!contentLine.isEmpty()){
             if(contentLine.contains("=") || contentLine.contains("/")){
-                data.get(questionCount).add(cleanData(contentLine));
+                data.get(questionCount).add(contentLine);
 
             }
             else{
@@ -16,9 +15,6 @@ public class ReadQuestionAndAnswerData extends ReadData{
                 
             }
         }   
-    }
-    private String cleanData(String text) {
-        return text.replaceAll("\\s+","").toLowerCase();
     }
     public ReadQuestionAndAnswerData(String fileName){
         super(fileName);
