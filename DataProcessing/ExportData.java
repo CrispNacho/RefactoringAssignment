db7ac7a218d7c004b0f3961ff7be2352c390d450
Neethan Rajadurai <neethan2005@gmail.com>
2022-11-21 15:36:23 -0500

    Refactoring Export Data

--- DataProcessing/ExportData.java
+++ DataProcessing/ExportData.java
@@ -14,15 +14,20 @@ public class ExportData {
    * *@param scoreArray is a two dimentional array that has student data but, with the student scores (adding the student score to the end of the array)
    * */
 	//Takes the score array where the student data is with the score and prints the array in a seperate file
-    public static void writingToScoreFile(ArrayList<ArrayList<String>> scoreArrayList) throws IOException{
+    public void writingToScoreFile(ArrayList<ArrayList<String>> scoreArrayList, String heading) {
 		//Establishes the file name
-		File file = new File("score.txt");
+		File file = new File("score.csv");
 		try{
 			FileWriter fw = new FileWriter(file);
 			Writer output = new BufferedWriter(fw);
-			int size = scoreArrayList.size();  
+			int size = scoreArrayList.size();
+			
+			output.write(heading);
 			for(int i = 0; i < size; i++){
-				output.write(scoreArrayList.get(i).toString() + "\n");  
+				for (int j = 0; j < scoreArrayList.get(i).size(); j++) {
+					output.write(scoreArrayList.get(i).get(j) + ",");
+				}
+				output.write("\n");  
 			}
 			output.close();
 		} catch (Exception e){
