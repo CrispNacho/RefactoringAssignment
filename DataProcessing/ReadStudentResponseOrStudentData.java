18d5a3bf482f990ba308a5cffd114f9fbbcac3b7
Neethan Rajadurai <neethan2005@gmail.com>
2022-11-21 19:53:05 -0500

    Added ReadStudentResponseOrStudentData.java

--- DataProcessing/ReadStudentResponseOrStudentData.java
+++ DataProcessing/ReadStudentResponseOrStudentData.java
@@ -2,15 +2,26 @@ package DataProcessing;
 import java.util.Arrays;
 import java.util.ArrayList;
 public class ReadStudentResponseOrStudentData extends ReadData{
+    /**
+   * *Adds the content line divides based off of the comma
+   * *@param contentLine is the data that will be divided based off of the comma
+   * */
     public void addContentLine(String contentLine){
-        
+        //Divides based off of the coma
         String regex = ",";
         ArrayList<String> contentRow = new ArrayList<String>(Arrays.asList(contentLine.split(regex)));
         data.add(contentRow);
     }
+
+    //Removes the header by removing the first index
     public void removeHeader(){
         data.remove(0);
     }
+
+    /**
+   * *Reads the student response or student data
+   * *@param contentLine is the data that will be read
+   * */
     public ReadStudentResponseOrStudentData(String fileName){
         super(fileName);
     }
