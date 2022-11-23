ac187520750ceee27ab5c25c318f7375778a4bfc
Oblivion Says Hello <lewinl349@gmail.com>
2022-11-22 22:43:46 -0500

    remove hillow

--- BridgeRefactoring.java
+++ BridgeRefactoring.java
@@ -2,8 +2,6 @@ import Data.Student;
 
 class BridgeRefactoring {
     public static void main(String[] args) {
-        System.out.println("hillo");
-
         Student student = new Student();
         }
 }
