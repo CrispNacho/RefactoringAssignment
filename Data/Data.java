609351a07ad634f4d0ee3f2f7cc6c22493ed93c8
CrispNacho <85505948+CrispNacho@users.noreply.github.com>
2022-11-22 22:51:12 -0500

    Delete Data.java

--- Data/Data.java
+++
@@ -1,9 +0,0 @@
-package Data;
-import DataProcessing.ValidateFile;
-import DataProcessing.ExportData;
-import java.util.ArrayList;
-
-abstract class Data {
-    protected ValidateFile validater = new ValidateFile();
-    protected ExportData exporter = new ExportData();
-}
